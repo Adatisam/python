6bec5639d7467196261144c410140ff725eecf9f
Matheus Adati Miguel <adati.miguel@gmail.com>
2024-08-28 22:49:40 -0300

    Create ex09.py

---
+++ atividade.de.python/ex09.py
@@ -0,0 +1,8 @@
+print('-=-' * 10)
+print('Analisador de Trângulos')
+print('-=-' * 10)
+lado1 = float(input('Primeiro segmento'))
+lado2 = float(input('Segundo segmento'))
+lado3 = float(input('Terceiro segmento'))
+
+
